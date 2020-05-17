649e29d4e6a0e36faa58c7130fd6855c35ac306a
Felipe Hernandez <felipe.hdz.wong@gmail.com>
2020-05-17 17:49:23 +0200

    Implement delete in ListFragment

--- app/src/main/java/com/example/favorite_places/adapter/PlaceRecyclerAdapter.java
+++ app/src/main/java/com/example/favorite_places/adapter/PlaceRecyclerAdapter.java
@@ -4,16 +4,20 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.favorite_places.R;
 import com.example.favorite_places.data.Place;
-import com.example.favorite_places.viewholder.PlaceViewHolder;
+import com.example.favorite_places.repo.PlaceRepo;
 
 import java.util.List;
 
-public class PlaceRecyclerAdapter extends RecyclerView.Adapter<PlaceViewHolder> {
+public class PlaceRecyclerAdapter extends RecyclerView.Adapter<PlaceRecyclerAdapter.ViewHolder> {
 
     private Context context;
     private int layoutResource ;
@@ -31,14 +35,14 @@ public class PlaceRecyclerAdapter extends RecyclerView.Adapter<PlaceViewHolder> 
 
     @NonNull
     @Override
-    public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // creates the 'ViewHolder' to be used for the whole list
         View view = LayoutInflater.from(context).inflate(layoutResource, parent, false);
-        return new PlaceViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PlaceViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         // allows to fill in the row widget with actual data
         holder.bindData(position, placeList.get(position));
     }
@@ -47,5 +51,38 @@ public class PlaceRecyclerAdapter extends RecyclerView.Adapter<PlaceViewHolder> 
     public int getItemCount() {
         // sets up the amount of elements to display on the list
         return placeList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ImageView imgPlace;
+        private ImageView imgDelete;
+        private TextView tvPlaceName;
+        private TextView tvPlaceDesc;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.imgPlace = itemView.findViewById(R.id.place_row__img_place);
+            this.imgDelete = itemView.findViewById(R.id.place_row__img_delete);
+            this.tvPlaceName = itemView.findViewById(R.id.place_row__tv_name);
+            this.tvPlaceDesc = itemView.findViewById(R.id.place_row__tv_desc);
+
+            imgDelete.setOnClickListener(this);
+        }
+
+        public void bindData(int position, Place place) {
+            imgPlace.setImageResource(R.drawable.ic_image_black_24dp);
+            imgDelete.setImageResource(R.drawable.ic_delete_black_24dp);
+            tvPlaceName.setText(place.getName());
+            tvPlaceDesc.setText(place.getDesc());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Place place = placeList.get(getAdapterPosition());
+            PlaceRepo placeRepo = new PlaceRepo(v.getContext());
+            placeRepo.deletePlace(place);
+            Toast.makeText(v.getContext(), "Deleted", Toast.LENGTH_SHORT).show();
+        }
     }
 }
